defe9e3bea00150f105f157a3d6161dadac2bc9d
CedricDeVijt <46058254+CedricDeVijt@users.noreply.github.com>
2025-09-14 20:21:16 +0200

    layout change to padding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,17 +41,17 @@ function Home() {
     return (
         <>
             <div>
-                <h1 className="pl-12 text-5xl font-extrabold mb-6 bg-clip-text drop-shadow-lg">Popular Movies</h1>
+                <h1 className="pl-12 pt-4 text-5xl font-extrabold mb-6 bg-clip-text drop-shadow-lg">Popular Movies</h1>
                 <MovieCarousel movies={popularMovies}/>
             </div>
 
             <div>
-                <h1 className="pl-12 text-5xl font-extrabold mb-6 bg-clip-text drop-shadow-lg">Top Rated Movies</h1>
+                <h1 className="pl-12 pt-4 text-5xl font-extrabold mb-6 bg-clip-text drop-shadow-lg">Top Rated Movies</h1>
                 <MovieCarousel movies={topRatedMovies}/>
             </div>
 
             <div>
-                <h1 className="pl-12 text-5xl font-extrabold mb-6 bg-clip-text drop-shadow-lg">Upcoming Movies</h1>
+                <h1 className="pl-12 pt-4 text-5xl font-extrabold mb-6 bg-clip-text drop-shadow-lg">Upcoming Movies</h1>
                 <MovieCarousel movies={upcomingMovies}/>
             </div>
 
