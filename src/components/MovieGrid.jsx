a9386199ed8be828d88fc0920e15520c6ca55544
CedricDeVijt <46058254+CedricDeVijt@users.noreply.github.com>
2025-09-14 22:15:54 +0200

    movies without poster wont be displayed

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -6,7 +6,10 @@ function MovieGrid({ movieIDs }) {
   // Fetch movie details based on movieIDs using getMovieDetails()
   let [movies, setMovies] = useState([]);
 
-  useEffect(() => {
+  const moviesWithImages = movies.filter((movie) => movie.poster_path);
+
+
+    useEffect(() => {
     async function fetchMovies() {
       const movieDetails = await Promise.all(
         movieIDs.map((id) => getMovieDetails(id)),
@@ -19,11 +22,11 @@ function MovieGrid({ movieIDs }) {
 
   return (
     <div className="w-full px-4">
-      {movies.length === 0 ? (
+      {moviesWithImages.length === 0 ? (
         <p className="text-center text-gray-500 p-8">No movies to display.</p>
       ) : (
         <div className="grid grid-cols-[repeat(auto-fit,minmax(208px,1fr))] gap-4 justify-center mx-auto max-w-full pt-4">
-          {movies.map((movie, idx) => (
+          {moviesWithImages.map((movie, idx) => (
             <MovieCard key={movie.id || idx} movie={movie} />
           ))}
         </div>
