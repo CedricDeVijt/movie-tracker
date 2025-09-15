d110e0498df4775cdde2cf5799d2143192cf4b25
CedricDeVijt <46058254+CedricDeVijt@users.noreply.github.com>
2025-09-15 13:31:21 +0200

    updated button logic, if movie is marked "watched" it will be removed from watchlist and the "Add to watchlist" button disappears

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -61,18 +61,20 @@ function MovieDetail({
         >
           {hasWatched(Number(id)) ? "Remove from Watched" : "Add to Watched"}
         </button>
-        <button
-          onClick={handleToggleWatchlist}
-          className={`px-3 py-2 rounded-md ${
-            hasInWatchlist(Number(id))
-              ? "bg-gray-200 text-blue-600 border border-blue-600 hover:bg-gray-300"
-              : "bg-blue-600 text-white hover:bg-blue-700"
-          }`}
-        >
-          {hasInWatchlist(Number(id))
-            ? "Remove from Watchlist"
-            : "Add to Watchlist"}
-        </button>
+        {!hasWatched(Number(id)) && (
+          <button
+            onClick={handleToggleWatchlist}
+            className={`px-3 py-2 rounded-md ${
+              hasInWatchlist(Number(id))
+                ? "bg-gray-200 text-blue-600 border border-blue-600 hover:bg-gray-300"
+                : "bg-blue-600 text-white hover:bg-blue-700"
+            }`}
+          >
+            {hasInWatchlist(Number(id))
+              ? "Remove from Watchlist"
+              : "Add to Watchlist"}
+          </button>
+        )}
       </div>
     </div>
   );
--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -25,6 +25,9 @@ function Movie() {
       removeWatched(Number(id));
     } else {
       addWatched(Number(id));
+      if (hasInWatchlist(Number(id))) {
+        removeFromWatchlist(Number(id));
+      }
     }
   }
 
